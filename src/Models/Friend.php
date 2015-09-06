60edd0209279c9f8405d7c011ddc8e99f031f226
Florian Wartner <f.wartner@ployin.co>
2015-09-06 14:03:53 +0200

    first commit

---
+++ src/Models/Friend.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of Laravel Friendable.
+ *
+ * (c) PackageBackup <hello@draperstud.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PackageBackup\Friendable\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Friend.
+ */
+class Friend extends Model
+{
+    /**
+     * @var string
+     */
+    public $table = 'friendships';
+
+    /**
+     * @var array
+     */
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function sender()
+    {
+        return $this->morphTo('sender');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function recipient()
+    {
+        return $this->morphTo('recipient');
+    }
+}
